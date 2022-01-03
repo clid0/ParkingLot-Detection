f42b9280c6cc106cadb47ae44a114003fc79bcf3
Minkeyyyy <invincible92@naver.com>
2022-01-03 23:45:09 +0900

    Update session

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import subprocess
 import os.path
 import gridfs
 
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, render_template, session, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "12312sdasdasd"
 
 client = MongoClient('mongodb://test:test@3.141.15.72', 27017)
 # client = MongoClient('localhost', 27017)
@@ -17,11 +18,22 @@ db = client.dbteamdi
 # HTML 화면 보여주기
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if "userID" in session:
+        return render_template('map.html', login =True)
+    else:
+        return render_template('index.html',login = False)
 
 @app.route('/map')
 def map():
     return render_template('map.html')
+
+@app.route('/logout',methods=['GET'])
+def logout():
+    session.pop("userID")
+    print(session)
+    return jsonify({'msg': '로그아웃!'})
+
+
 
 
 @app.route('/check',  methods=['POST'])
@@ -33,37 +45,19 @@ def check():
         'password': password_receive
     }
     a = db.clients.find_one({"email":email_receive})
-    # print('find_one')
-    # print(a)
+
     if a is not None:
+
         if a['email'] == email_receive and a['password'] == password_receive:
-            return jsonify({'msg': '로그인!','name':'map'})
-            # return jsonify({'msg': '로그인!'})
+            session["userID"] = email_receive
+            return redirect(url_for("map"))
         else:
             return jsonify({'msg': '패스워드 틀림!'})
     else:
         # render_template('html.html')
         return jsonify({'msg': '로그인실패!'})
 
-
-    # return render_template('index.html')
-
-# ## API 역할을 하는 부분
-# @app.route('/review', methods=['POST'])
-# def write_review():
-#     title_receive = request.form['title_give']
-#     author_receive = request.form['author_give']
-#     review_receive = request.form['review_give']
-#     #일단 딕셔너리 만듬
-#     doc = {
-#         'title' : title_receive,
-#         'author' : author_receive,
-#         'review' : review_receive
-#     }
-#     #db에 doc을 저장
-#     db.bookreview.insert_one(doc)
-#     return jsonify({'msg': '저장완료!'})
-
+    return render_template('index.html')
 
 @app.route('/api/zones', methods=['GET'])
 def all_zones():
@@ -79,11 +73,6 @@ def open_img():
     }
     zone = db.socarzone.find_one({'zone_name' : name_receive},{'_id':False})
 
-    # detect.py -> max_car update -> data
-    # db.socarzone.update()
-    # zone['index'] 활용
-    #
-    # model = '--weights dkd.pt'
 
     zone_name = zone['zone_name']
     print("zone_name",zone_name)
@@ -115,8 +104,6 @@ def open_img():
     else:
         return jsonify({"img_url": "없습니다"})
 
-
-    # img_url = '../static/' +name_receive+'/'+ 'm_icon.jpg'#여기가 핵심임.
 
 
 
